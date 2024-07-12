44782c6b73165f7dd26c2042e76109d8e9248470
kshitijyejare <91865080+kshitijyejare@users.noreply.github.com>
2024-07-12 23:48:08 +0530

    Update tool1.py

--- tool1.py
+++ tool1.py
@@ -3,3 +3,12 @@ import bs4
 import requests
 import openpyxl
 import pandas as pd
+
+baseurl = 'http://www.puneapmc.org/rates.aspx'
+
+r = requests.get('http://www.puneapmc.org/history.aspx?id=Rates3997')
+soup = BeautifulSoup(r.content,'lxml')
+
+productlist = soup.find_all('div',class_='item')
+
+print(productlist)
